69f1bb7f2d272a544f572003fc5b2ad0196dbe18
MatiasRobles <matiasmax@gmail.com>
2022-07-09 16:56:15 -0300

    Added messages as logs.

--- Program.cs
+++ Program.cs
@@ -18,17 +18,23 @@ namespace PoCSteganography
             {
                 using (Image<Rgba32> inputImage = Image.Load<Rgba32>(imageFile))
                 {
+                    Console.WriteLine("Read image file...");
                     using (StreamReader sr = File.OpenText(textFile))
                     {
                         string text = sr.ReadToEnd();
+                        Console.WriteLine("Read text file...");
 
                         if (inputImage.Height * inputImage.Width * 3 / 8 >= text.Length + 1)
                         {
                             if (!string.IsNullOrEmpty(password))
                                 text = AESEncryptionHelper.Encrypt(text, password);
 
+                            Console.WriteLine("Start the main process HIDE...");
                             Image outImage = SteganographyHelper.EmbedText(text, inputImage);
+                            Console.WriteLine("Finished the main process HIDE.");
+
                             outImage.SaveAsPng("hidden.png");
+                            Console.WriteLine("Your output image file is: hidden.png");
                         }
                         else
                             Console.WriteLine("Espacio insuficente para almacenar el texto. Se requiere una imagen de mayores dimensiones");
@@ -36,25 +42,28 @@ namespace PoCSteganography
                 }
             }
             else
-                Console.WriteLine("Archivo no encontrado");
+                Console.WriteLine("File not found");
         }
 
         public void Unhide([Option('f')] string file, [Option('p')] string password = "")
         {
             if (File.Exists(file))
             {
-                using (Image<Rgba32> inputImage = Image.Load<Rgba32>(file))
-                {
-                    string text = SteganographyHelper.ExtractText(inputImage);
+                using Image<Rgba32> inputImage = Image.Load<Rgba32>(file);
+                Console.WriteLine("Read image file...");
 
-                    if (!string.IsNullOrEmpty(password))
-                        text = AESEncryptionHelper.Decrypt(text, password);
+                Console.WriteLine("Start the main process UNHIDE...");
+                string text = SteganographyHelper.ExtractText(inputImage);
 
-                    File.WriteAllText("secret.txt", text);
-                }
+                if (!string.IsNullOrEmpty(password))
+                    text = AESEncryptionHelper.Decrypt(text, password);
+                Console.WriteLine("Finished the main process UNHIDE...");
+
+                File.WriteAllText("secret.txt", text);
+                Console.WriteLine("Your output text file is: secret.txt");
             }
             else
-                Console.WriteLine("Archivo no encontrado");
+                Console.WriteLine("File not found");
         }
     }
 }
